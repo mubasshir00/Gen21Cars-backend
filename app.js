16f6912a410840d2648a897e080e1916ffb24e95
Mubasshir Ahmed <mubasshir.ahmed@northsouth.edu>
2021-10-16 04:11:53 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express();
+const morgan = require('morgan')
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const errorHandler = require('./helpers/error-handler')
+const authJwt = require('./helpers/jwt')
+
+app.use(cors());
+app.options('*',cors())
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use(errorHandler);
+
+const api = process.env.API_URL;
+
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+
+//Routers
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'Gen21Cars'
+})
+.then(() => {
+    console.log('Database Connection is ready...')
+})
+.catch((err) => {
+    console.log(err);
+})
+
+app.listen(3000,()=>{
+    console.log('Running 3000');
+})
---
+++ userdata.js
@@ -0,0 +1,214 @@
+[
+    {
+        "_id": "61661baf840551598de48583",
+        "name": "Rakib",
+        "email": "erere@gmail.com",
+        "phone": "2323232",
+        "isAdmin": true,
+        "street": "",
+        "apartment": "apartment 333",
+        "zip": "3432",
+        "city": "ed weds w",
+        "country": "USA",
+        "__v": 0,
+        "id": "61661baf840551598de48583"
+    },
+    {
+        "_id": "61661c1c840551598de48585",
+        "name": "hjh hjjhj hyh",
+        "email": "erere@gmail.com",
+        "phone": "2323232",
+        "isAdmin": true,
+        "street": "",
+        "apartment": "apartment 333",
+        "zip": "3432",
+        "city": "ed weds w",
+        "country": "USA",
+        "__v": 0,
+        "id": "61661c1c840551598de48585"
+    },
+    {
+        "_id": "61661c23840551598de48587",
+        "name": "pppppppppp hjjhj hyh",
+        "email": "erere@gmail.com",
+        "phone": "2323232",
+        "isAdmin": true,
+        "street": "",
+        "apartment": "apartment 333",
+        "zip": "3432",
+        "city": "ed weds w",
+        "country": "USA",
+        "__v": 0,
+        "id": "61661c23840551598de48587"
+    },
+    {
+        "_id": "61661ea4d00084c03607e85a",
+        "name": "rrrrrrrr hjjhj hyh",
+        "email": "erere@gmail.com",
+        "phone": "2323232",
+        "isAdmin": true,
+        "street": "",
+        "apartment": "apartment 333",
+        "zip": "3432",
+        "city": "ed weds w",
+        "country": "USA",
+        "__v": 0,
+        "id": "61661ea4d00084c03607e85a"
+    },
+    {
+        "_id": "6166edbc0de99a7c2daa030d",
+        "name": "rrrrrrrr hjjhj hyh",
+        "email": "erere@gmail.com",
+        "phone": "2323232",
+        "isAdmin": true,
+        "street": "dde",
+        "apartment": "apartment 333",
+        "zip": "3432",
+        "city": "ed weds w",
+        "country": "USA",
+        "__v": 0,
+        "id": "6166edbc0de99a7c2daa030d"
+    },
+    {
+        "_id": "6166edcc0de99a7c2daa030f",
+        "name": "rakib hjjhj hyh",
+        "email": "rakib@gmail.com",
+        "phone": "23234554232",
+        "isAdmin": true,
+        "street": "dde",
+        "apartment": "apartment 333",
+        "zip": "3432",
+        "city": "ed weds w",
+        "country": "USA",
+        "__v": 0,
+        "id": "6166edcc0de99a7c2daa030f"
+    },
+    {
+        "_id": "6166f6d41de1af45d2a993b2",
+        "name": "abc edf",
+        "email": "abc@gmail.com",
+        "phone": "+32322",
+        "isAdmin": false,
+        "street": "ewe",
+        "apartment": "sd wdewe",
+        "zip": "454",
+        "city": "we we",
+        "country": "bangladesh",
+        "__v": 0,
+        "id": "6166f6d41de1af45d2a993b2"
+    },
+    {
+        "_id": "6166f7281de1af45d2a993b4",
+        "name": "anika",
+        "email": "anika@gmail.com",
+        "phone": "+32322",
+        "isAdmin": false,
+        "street": "ewe",
+        "apartment": "sd wdewe",
+        "zip": "454",
+        "city": "we we",
+        "country": "bangladesh",
+        "__v": 0,
+        "id": "6166f7281de1af45d2a993b4"
+    },
+    {
+        "_id": "6166f7721de1af45d2a993b6",
+        "name": "anika",
+        "email": "anika@gmail.com",
+        "phone": "+32322",
+        "isAdmin": false,
+        "street": "ewe",
+        "apartment": "sd wdewe",
+        "zip": "454",
+        "city": "we we",
+        "country": "bangladesh",
+        "__v": 0,
+        "id": "6166f7721de1af45d2a993b6"
+    },
+    {
+        "_id": "616716e8e3677e8fcf8861eb",
+        "name": "anika",
+        "email": "anikat@gmail.com",
+        "phone": "+343",
+        "isAdmin": false,
+        "street": "",
+        "apartment": "",
+        "zip": "",
+        "city": "",
+        "country": "",
+        "__v": 0,
+        "id": "616716e8e3677e8fcf8861eb"
+    },
+    {
+        "_id": "61671726e3677e8fcf8861ed",
+        "name": "anikatab",
+        "email": "anikatab@gmail.com",
+        "phone": "+343",
+        "isAdmin": false,
+        "street": "",
+        "apartment": "",
+        "zip": "",
+        "city": "",
+        "country": "",
+        "__v": 0,
+        "id": "61671726e3677e8fcf8861ed"
+    },
+    {
+        "_id": "61671736e3677e8fcf8861ef",
+        "name": "anika",
+        "email": "anika@gmail.com",
+        "phone": "+343",
+        "isAdmin": false,
+        "street": "",
+        "apartment": "",
+        "zip": "",
+        "city": "",
+        "country": "",
+        "__v": 0,
+        "id": "61671736e3677e8fcf8861ef"
+    },
+    {
+        "name": "anikatabassum_momo",
+        "email": "anikatabassum_momo@gmail.com",
+        "password": "anika123",
+        "phone": "+343",
+        "isAdmin": true,
+        "street": "",
+        "apartment": "",
+        "zip": "",
+        "city": "",
+        "country": "",
+        "user": "anikatabassum_momo@gmail.com",
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTY5YmQ3ZWU2NzQ5MDhkNjc1Y2E3ZmIiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2MzQzMjEwNDcsImV4cCI6MTYzNDkyNTg0N30._o_ETcesSaMfe3KDG2HiMbXiS1tCXE5Sttmemi_Q1ig"
+    }
+]
+
+// {
+//     "user": "anika@gmail.com"
+//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MTY2ZjcyODFkZTFhZjQ1ZDJhOTkzYjQiLCJpYXQiOjE2MzQxNDY2Nzl9.mCtZMEKyLOITgLHco88G_Q2qEUAJoGr2vAS668fibJs"
+// }
+
+
+
+const orderdata =[
+    {
+        "orderItems": [
+            {
+                "quantity": 3,
+                "product": "6165ea3805c8cf34d51358ff"
+            },
+            {
+                "quantity": 3,
+                "product": "6165eaf19bba35b0fa654195"
+            }
+        ],
+        "shippingAddress1": "Nai",
+        "shippingAddress2": "ctg",
+        "city": "Dhaka",
+        "zip": "wewew",
+        "country": "BD",
+        "phone": "+343434",
+        "status": "ddfd",
+        "totalPrice": 454
+    }
+]
